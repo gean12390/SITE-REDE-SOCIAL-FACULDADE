d27950f65a27140437d9791a72558aff01b0f9a2
Giovanni Pascuotte da Silva <100391366+gean12390@users.noreply.github.com>
2024-05-12 22:30:02 -0300

    Add files via upload

---
+++ botao.js
@@ -0,0 +1,8 @@
+function sg(){
+
+	let seguir = document.getElementById('seguir');
+
+	seguir.value = "SEGUINDO";
+	seguir.style = "background-color: rgba(169, 169, 169, 0.5); color:white;"
+
+}
---
+++ menu.js
@@ -0,0 +1,25 @@
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+        function openNav() {
+          document.getElementById("mySidebar").style.width = "250px";
+          document.getElementById("main").style.marginLeft = "250px";
+        }
+        
+        /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+        function closeNav() {
+          document.getElementById("mySidebar").style.width = "0";
+          document.getElementById("main").style.marginLeft = "0";
+        }          
+
+
+  /* Define a largura do menu lateral para 250px e a margem direita do conteúdo da página para 250px */
+            function openCustomNav() {
+                document.getElementById("customSidebar").style.width = "400px";
+                document.getElementById("main-content").style.marginRight = "400px";
+            }
+        
+            /* Define a largura do menu lateral para 0 e a margem direita do conteúdo da página para 0 */
+            function closeCustomNav() {
+                document.getElementById("customSidebar").style.width = "0";
+                document.getElementById("main-content").style.marginRight = "0";
+            }
+     
